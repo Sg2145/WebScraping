2908fe2ec98d27c341dbf5d41771820cda240dff
Sg2145 <117072632+Sg2145@users.noreply.github.com>
2022-12-05 00:50:04 +0530

    Add files via upload

--- main2.py
+++ main2.py
@@ -15,11 +15,11 @@ enter_button = driver.find_element("xpath", '//a[@class="desktop-submit"]')
 enter_button.click()
 list1 = driver.find_elements(By.CLASS_NAME, "product-base")
 for match in list1:
-    print(match.text)
+    text = match.find_element(By.CLASS_NAME, "product-product")
     word = driver.find_element("xpath", '//h4[@class="product-product"]').text
-    if "Sneakers" in word:
-        sneaker_name.append(driver.find_element(
-            "xpath", '//h4[@class="product-product"]').text)
+    if "Sneakers" in text.text:
+        sneaker_name.append(text.text)
+
 
 print("page1ends")
 page_button2 = driver.find_element(
@@ -27,32 +27,31 @@ page_button2 = driver.find_element(
 page_button2.click()
 list2 = driver.find_elements(By.CLASS_NAME, "product-base")
 for match2 in list2:
-    print(match2.text)
-    word2 = driver.find_element("xpath", '//h4[@class="product-product"]').text
-    if "Sneakers" in word2:
-        sneaker_name.append(driver.find_element(
-            "xpath", '//h4[@class="product-product"]').text)
+    text = match2.find_element(By.CLASS_NAME, "product-product")
+    word = driver.find_element("xpath", '//h4[@class="product-product"]').text
+    if "Sneakers" in text.text:
+        sneaker_name.append(text.text)
+        # sneaker_name.append(driver.find_element(
+        # "xpath", '//h4[@class="product-product"]').text)
 print("page2ends")
 page_button3 = driver.find_element(
     "xpath", '//a[@href="https://www.myntra.com/shoes?p=3"]')
 page_button3.click()
 list3 = driver.find_elements(By.CLASS_NAME, "product-base")
 for match3 in list3:
-    print(match3.text)
-    word3 = driver.find_element("xpath", '//h4[@class="product-product"]').text
-    if "Sneakers" in word3:
-        sneaker_name.append(driver.find_element(
-            "xpath", '//h4[@class="product-product"]').text)
+    text = match3.find_element(By.CLASS_NAME, "product-product")
+    word = driver.find_element("xpath", '//h4[@class="product-product"]').text
+    if "Sneakers" in text.text:
+        sneaker_name.append(text.text)
 print("page3ends")
 page_button4 = driver.find_element(
     "xpath", '//a[@href="https://www.myntra.com/shoes?p=4"]')
 page_button4.click()
 list4 = driver.find_elements(By.CLASS_NAME, "product-base")
 for match4 in list4:
-    print(match4.text)
-    word4 = driver.find_element("xpath", '//h4[@class="product-product"]').text
-    if "Sneakers" in word4:
-        sneaker_name.append(driver.find_element(
-            "xpath", '//h4[@class="product-product"]').text)
+    text = match4.find_element(By.CLASS_NAME, "product-product")
+    word = driver.find_element("xpath", '//h4[@class="product-product"]').text
+    if "Sneakers" in text.text:
+        sneaker_name.append(text.text)
 print("page4ends")
 print(sneaker_name)
